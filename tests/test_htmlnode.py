22d155df474bc9f1e82021e80d450bcde9c993b1
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-06-28 20:03:59 -0400

    update HTMLNode tests

--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -3,7 +3,7 @@ from src.htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
-        node = HTMLNode(tag="h1", value="this is a heading")
+        node = HTMLNode(tag='h1', value='this is a heading')
         self.assertEqual(
             repr(node),
             "HTMLNode(tag='h1', value='this is a heading', children=None, props=None)"
@@ -15,6 +15,13 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNone(node.value)
         self.assertIsNone(node.children)
         self.assertIsNone(node.props)
+
+    def test_props_to_html(self):
+        node = HTMLNode(tag='a', props={'href': 'https://www.boot.dev', 'target': '_blank'})
+        self.assertEqual(
+            node.props_to_html(),
+            ' href="https://www.boot.dev" target="_blank"'
+        )
 
 
 
