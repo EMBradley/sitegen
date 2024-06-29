da616dabaaeee35d6e1cc6dd2315914a22470aa7
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-06-29 10:06:47 -0400

    move split_nodes and extract to main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@
 This package was created as part of the author's completion of Boot.dev's
 backend developer course.
 """
+import re
+from typing import List, Tuple
 from textnode import TextNode, TextType
 
 
@@ -11,5 +13,54 @@ def main():
     node = TextNode("hello world", TextType.Bold, "https://www.boot.dev")
     print(node)
 
+def split_nodes(nodes: List[TextNode], delimiter: str, text_type: TextType) -> List[TextNode]:
+    """
+    Splits each of the `Text` nodes in `nodes`, and inserts nodes of type `text_type`
+    between each pair of `delimiters`
+    """
+    new_nodes = []
 
-main()
+    for node in nodes:
+        if node.text_type != TextType.Normal:
+            new_nodes.append(node)
+            continue
+        if not delimiter in node.text:
+            new_nodes.append(node)
+            continue
+
+        chunks = node.text.split(delimiter)
+
+        # an even number of delimiters splits the text into an odd number of chunks,
+        # so if there are an even number of chunks then there are mismatched delimiters
+        if len(chunks) % 2 == 0:
+            raise ValueError("Unclosed delimiter found")
+
+        for (i, chunk) in enumerate(chunks):
+            if not chunk:
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(chunk, TextType.Normal))
+            else:
+                new_nodes.append(TextNode(chunk, text_type))
+
+
+    return new_nodes
+
+def extract_markdown_images(text: str) -> List[Tuple[str, str]]:
+    """
+    Finds all markdown images in given text. Images are represented in markdown by strings
+    of the form ![alt text](link to image). This function returns all images in the given text 
+    in the form of a list of (alt text, link) tuples.
+    """
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text: str) -> List[Tuple[str, str]]:
+    """
+    Finds all markdown links in given text. Images are represented in markdown by strings
+    of the form [link text](url). This function returns all images in the given text
+    in the form of a list of (link text, url) tuples.
+    """
+    return re.findall(r"(?:[^!])\[(.*?)\]\((.*?)\)", text)
+
+if __name__ == "__main__":
+    main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,13 +3,7 @@
 # pylint: disable=missing-function-docstring
 
 import unittest
-from textnode import (
-        TextNode,
-        TextType,
-        extract_markdown_images,
-        extract_markdown_links,
-        split_nodes
-)
+from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
@@ -97,133 +91,6 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             img_html,
             '<img src="https://bit.ly/4bx5bzq" alt="Autism creature"></img>',
-        )
-
-
-class TestSplitNode(unittest.TestCase):
-    def test_split_nodes_code(self):
-        node_with_code_block = TextNode("This is text with a `code block` word", TextType.Normal)
-        new_nodes = split_nodes([node_with_code_block], "`", TextType.Code)
-        self.assertEqual(
-            new_nodes,
-            [
-                TextNode("This is text with a ", TextType.Normal),
-                TextNode("code block", TextType.Code),
-                TextNode(" word", TextType.Normal),
-            ]
-        )
-
-    def test_split_nodes_bold(self):
-        node_with_bold = TextNode(
-            "This node has **bold text** and some **more** bold text",
-            TextType.Normal
-        )
-        new_nodes = split_nodes([node_with_bold], "**", TextType.Bold)
-        self.assertEqual(
-            new_nodes,
-            [
-                TextNode("This node has ", TextType.Normal),
-                TextNode("bold text", TextType.Bold),
-                TextNode(" and some ", TextType.Normal),
-                TextNode("more", TextType.Bold),
-                TextNode(" bold text", TextType.Normal)
-            ]
-        )
-
-    def test_split_nodes_mixed(self):
-        mixed_type = TextNode(
-            "This node has a `code block`, some **bold text**, and some *italicized text*",
-            TextType.Normal
-        )
-        new_nodes = split_nodes([mixed_type], '`', TextType.Code)
-        new_nodes = split_nodes(new_nodes, "**", TextType.Bold)
-        new_nodes = split_nodes(new_nodes, "*", TextType.Italic)
-        self.assertEqual(
-            new_nodes,
-            [
-                TextNode("This node has a ", TextType.Normal),
-                TextNode("code block", TextType.Code),
-                TextNode(", some ", TextType.Normal),
-                TextNode("bold text", TextType.Bold),
-                TextNode(", and some ", TextType.Normal),
-                TextNode("italicized text", TextType.Italic)
-            ]
-        )
-
-
-class TestExtract(unittest.TestCase):
-    def test_extract_images(self):
-        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_images(text),
-            [
-                (
-                    "image",
-                    "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png" # pylint: disable=line-too-long
-                ),
-                (
-                    "another",
-                    "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png" # pylint: disable=line-too-long
-                )
-            ]
-        )
-
-    def test_extract_no_images(self):
-        no_images = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_images(no_images),
-            []
-        )
-
-    def test_no_extract_partial_image(self):
-        partial_image = "This text has ![alt text] without an image url and a !(url.com) with no alt text" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_images(partial_image),
-            []
-        )
-
-    def test_extract_only_image_when_link_present(self):
-        image_and_link = "This text has ![an image](https://www.test.com) and [a link](https://www.example.com)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_images(image_and_link),
-            [("an image", "https://www.test.com")]
-        )
-
-    def test_extract_links(self):
-        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_links(text),
-            [
-                (
-                    "link",
-                    "https://www.example.com"
-                ),
-                (
-                    "another",
-                    "https://www.example.com/another"
-                )
-            ]
-        )
-
-    def test_extract_no_links(self):
-        no_link = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_links(no_link),
-            []
-        )
-
-    def test_no_extract_partial_link(self):
-        partial_link = "This text has a [link name] and (url.com) but not together"
-        self.assertEqual(
-            extract_markdown_links(partial_link),
-            []
-        )
-
-    def test_extract_only_link_when_image_present(self):
-        image_and_link = "This text has ![an image](https://www.test.com) and [a link](https://www.example.com)" # pylint: disable=line-too-long
-        self.assertEqual(
-            extract_markdown_links(image_and_link),
-            [("a link", "https://www.example.com")]
         )
 
 
