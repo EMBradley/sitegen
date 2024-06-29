91a0dc88a3402a54afda1b33adeffaf6ca13331f
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-06-28 21:30:07 -0400

    add ParentNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,11 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, props=None, children=None):
+    def __init__(
+            self,
+            tag: str | None = None,
+            value: str | None = None,
+            props: dict | None = None,
+            children: list | None = None
+        ):
         self.tag = tag
         self.value = value
         self.props = props
@@ -23,13 +29,17 @@ class HTMLNode:
         return html_props
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(
+            self,
+            tag: str | None,
+            value: str,
+            props: dict | None = None
+        ):
         super().__init__(tag, value, props)
 
     def to_html(self):
         if not self.value:
             raise ValueError("LeafNode must have a value")
-
         if not self.tag:
             return self.value
 
@@ -38,3 +48,31 @@ class LeafNode(HTMLNode):
             html_props = self.props_to_html()
 
         return f"<{self.tag}{html_props}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str,
+            children: list,
+            props: dict | None = None
+        ):
+        super().__init__(tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+
+        html_props = ""
+        if self.props:
+            html_props = self.props_to_html()
+
+        opening_tag = f"<{self.tag}{html_props}>"
+        closing_tag = f"</{self.tag}>"
+        
+        children = ""
+        for child in self.children:
+            children += child.to_html()
+
+        return f"{opening_tag}{children}{closing_tag}"
--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from src.htmlnode import HTMLNode, LeafNode
+from src.htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -26,11 +26,11 @@ class TestHTMLNode(unittest.TestCase):
 
 class TestLeafNode(unittest.TestCase):
     def test_no_value(self):
-        node = LeafNode()
+        node = LeafNode(None, None) # type: ignore
         self.assertRaises(ValueError, node.to_html)
 
     def test_no_tag(self):
-        node = LeafNode(None, "this node has no tag")
+        node = LeafNode(None, "this node has no tag") # type: ignore
         self.assertEqual(node.to_html(), "this node has no tag")
 
     def test_with_tag(self):
@@ -39,6 +39,137 @@ class TestLeafNode(unittest.TestCase):
 
         self.assertEqual(node1.to_html(), "<p>This is a paragraph of text.</p>")
         self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+
+class TestParentNode(unittest.TestCase):
+    def test_no_tag(self):
+        node = ParentNode(None, [LeafNode(None, "some text")]) # type: ignore
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_no_children(self):
+        node_none_children = ParentNode("p", None) # type: ignore
+        node_empty_children = ParentNode("p", [])
+
+        self.assertRaises(ValueError, node_none_children.to_html)
+        self.assertRaises(ValueError, node_empty_children.to_html)
+
+    def test_nested_broken_inside(self):
+        node_none_children_inside = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                ParentNode("ul", None), # type: ignore
+                LeafNode("i", "italic text"),
+            ]
+        )
+        node_empty_children_inside = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode("ol", []),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+            ]
+        )
+        node_no_tag_inside = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode(None, [LeafNode(None, "some text")]), # type: ignore
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+            ]
+        )
+
+
+        self.assertRaises(ValueError, node_none_children_inside.to_html)
+        self.assertRaises(ValueError, node_empty_children_inside.to_html)
+        self.assertRaises(ValueError, node_no_tag_inside.to_html)
+
+
+    def test_single_layer(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        )
+
+    def test_nested(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode(
+                    "ol",
+                    [
+                        LeafNode("li", "a numbered list item"),
+                        LeafNode("li", "a second list item"),
+                    ]
+                ),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                ParentNode(
+                    "ul",
+                    [
+                        LeafNode("li", "a bullet point"),
+                        LeafNode("li", "another bullet point"),
+                    ]
+                ),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b><ol><li>a numbered list item</li>\
+<li>a second list item</li></ol>Normal text<i>italic text</i><ul>\
+<li>a bullet point</li><li>another bullet point</li></ul>Normal text</p>"
+        )
+
+    def test_three_levels(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                ParentNode(
+                    "ol",
+                    [
+                        LeafNode("li", "a numbered list item"),
+                        LeafNode("li", "a second list item"),
+                        ParentNode("li", [LeafNode("i", "an italicized list item")])
+                    ]
+                ),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                ParentNode(
+                    "ul",
+                    [
+                        LeafNode("li", "a bullet point"),
+                        LeafNode("li", "another bullet point"),
+                    ]
+                ),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b><ol><li>a numbered list item</li>\
+<li>a second list item</li><li><i>an italicized list item</i></li>\
+</ol>Normal text<i>italic text</i><ul><li>a bullet point</li>\
+<li>another bullet point</li></ul>Normal text</p>"
+        )
+
 
 
 if __name__ == "__main__":
