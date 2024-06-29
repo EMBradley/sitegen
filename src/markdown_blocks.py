5d1746f4c2ec4187a1416cd59ccf2abc7fc665ae
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-06-29 17:37:42 -0400

    add get_block_type for markdown blocks

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -2,6 +2,36 @@
 Provides functionality for converting markdown documents to blocks for further processing.
 """
 
+from enum import Enum
+import re
+
+BlockType = Enum(
+    "BlockType",
+    ["Paragraph", "Heading", "Code", "Quote", "UnorderedList", "OrderedList"],
+)
+
+
+def get_block_type(block: str) -> BlockType:
+    """
+    Determines the block type of a markdown block
+    """
+    if re.match(r"^#{1,6} .*", block):
+        return BlockType.Heading
+
+    lines = block.split("\n")
+    if lines and re.match(r"^```", lines[0]) and re.search(r"```$", lines[-1]):
+        return BlockType.Code
+    if all(re.match(r"^> ", line) for line in lines):
+        return BlockType.Quote
+    if all(re.match(r"^[*-] ", line) for line in lines):
+        return BlockType.UnorderedList
+    if all(
+        re.match(r"^" + str(i + 1) + r"\. .*", line) for (i, line) in enumerate(lines)
+    ):
+        return BlockType.OrderedList
+
+    return BlockType.Paragraph
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     """
@@ -10,6 +40,6 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     """
     blocks = markdown.split("\n\n")
     blocks = map(lambda block: block.strip(), blocks)
-    blocks = filter(bool, blocks)
+    blocks = filter(lambda block: block != "", blocks)
 
     return list(blocks)
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -5,12 +5,13 @@
 
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, get_block_type, markdown_to_blocks
 
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        markdown = """This is a **bolded** paragraph
+        markdown = """
+This is a **bolded** paragraph
 
 This is another paragraph with *italic* text and `code` here
 This is the same paragraph on a new line
@@ -37,6 +38,67 @@ Some of those lines have extra whitespace"""
                 "Some of those lines have extra whitespace",
             ],
         )
+
+    def test_block_type(self):
+        blocks = [
+            (
+                BlockType.Heading,
+                [
+                    "# Heading 1",
+                    "## Heading 2",
+                    "### Heading 3",
+                    "#### Heading 4",
+                    "##### Heading 5",
+                    "###### Heading 6",
+                ],
+            ),
+            (
+                BlockType.Code,
+                [
+                    "```this is a code block```",
+                    "```\nthis is a multiline code block\n```",
+                ],
+            ),
+            (
+                BlockType.Quote,
+                [
+                    "> It is a truth universally acknowledged",
+                    "> Roses are red\n> Violets are blue\n> Markdown is easy\n> HTML is too",
+                ],
+            ),
+            (
+                BlockType.UnorderedList,
+                [
+                    "* this is\n* an unordered\n* list",
+                    "- this list\n- has different\n- bullet points",
+                    "- this list\n* uses mixed\n* bullet point\n- styles",
+                ],
+            ),
+            (
+                BlockType.OrderedList,
+                [
+                    "1. This list\n2. Has numbered items\n3. Instead of bullet points",
+                    "1. This ordered list has only one item",
+                ],
+            ),
+            (
+                BlockType.Paragraph,
+                [
+                    "",  # there shouldn't be empty blocks, but if there are they should be paragraphs
+                    "This is a normal paragraph",
+                    "####### This has too many #'s to be a heading",
+                    "> This block\n* mixes quotes and unordered lists\n- so it is neither",
+                    ">This quote block\n>is missing spaces after lead characters",
+                    "1. This ordered list\n3. is out of order",
+                    "2. This ordered list\n3. doesn't start at 1",
+                    "1. This ordered list\n* has an\n- unordered list\n2. in the middle",
+                ],
+            ),
+        ]
+
+        for block_type, blocks in blocks:
+            for block in blocks:
+                self.assertEqual(get_block_type(block.strip()), block_type)
 
 
 if __name__ == "__main__":
